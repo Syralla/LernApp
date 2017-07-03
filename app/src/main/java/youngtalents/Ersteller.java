34807df73e5659cbdd364e0af4158e8a71528f28
Syralla <simon@theweleit.de>
2017-07-03 14:38:30 +0200

    Aufgaben Erstellung

--- app/src/main/java/youngtalents/Ersteller.java
+++ app/src/main/java/youngtalents/Ersteller.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.TextView;
 
 /**
  * Created by Simon on 01.07.2017.
@@ -22,10 +23,22 @@ public class Ersteller extends Activity {
     CheckBox eins;
     CheckBox zwei;
     CheckBox drei;
+    TextView txt;
+    int anforderungen;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.button);
+        plus = (CheckBox) findViewById(R.id.PPlus);
+        min = (CheckBox) findViewById(R.id.Minus);
+        mul = (CheckBox) findViewById(R.id.Mal);
+        div = (CheckBox) findViewById(R.id.Geteilt);
+        eins = (CheckBox) findViewById(R.id.Ein);
+        zwei = (CheckBox) findViewById(R.id.Zwei);
+        drei = (CheckBox) findViewById(R.id.Drei);
+        txt = (TextView) findViewById(R.id.aufgabenpaket_view);
 
 
 
@@ -35,10 +48,19 @@ public class Ersteller extends Activity {
             public void onClick(View v) {
 
 
+                if(checkkat() == true && checkste() == true){ //Nur wenn eine Kategorie und eine Stelle ausgewählt ist, passiert etwas.
 
+                }
+                else{
+                    if(checkkat() == true){
+                        txt.setText("Bitte wähle noch eine Anzahl von Stellen aus!");
+                    }
+                    else if(checkste() == true){
+                        txt.setText("Bitte wähle noch eine Rechenart aus!");
 
-
-
+                    }
+                    
+                }
 
             }
         });
@@ -48,5 +70,30 @@ public class Ersteller extends Activity {
 
 
 
+    }
+
+
+    public  boolean checkkat(){ //Methode um zu checken ob mindestens eine Kategorie ausgewählt wurde
+
+
+
+        if(plus.isChecked() == true || min.isChecked() == true || mul.isChecked() == true || div.isChecked() == true){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public  boolean checkste(){ // methode um zu checken ob mindestens eine Stellen Anzahl ausgewählt wurde
+
+
+
+        if(eins.isChecked() == true || zwei.isChecked() == true || drei.isChecked() == true ){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
